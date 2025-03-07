b69351a21fabed51bfb8dabbfaf61e3e5faec963
dima <dmitrysukhov96@gmail.com>
2025-03-07 09:23:33 +0200

    sdk = 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.skover.trainingandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.skover.trainingandroid"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
