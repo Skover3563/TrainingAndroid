e92339269708b6a92b0e477ece5a7676ffc69f33
Skover3563 <kostiaostrouhov6e@gmail.com>
2025-03-19 00:17:15 +0200

    Исправил

--- app/src/main/java/com/skover/trainingandroid/tasks/ColumnTasks.kt
+++ app/src/main/java/com/skover/trainingandroid/tasks/ColumnTasks.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.skover.trainingandroid.R
 
+
 //запускать приложение не надо.
 // надо открыть тут в правом верхнем углу вкладку предпросмотра
 
@@ -37,51 +38,62 @@ import com.skover.trainingandroid.R
 @Composable
 fun Task1() {
     // TODO: Сделай 3 текста которые располагаются сверху вниз.
-    Text(text = "Вверх")
-    Spacer(modifier = Modifier.height(10.dp))
-    Text(text = "Середина ")
-    Spacer(modifier = Modifier.height(10.dp))
-    Text(text = "Низ")
-    //тебе надо проверять задания в превью внизу. в этом задании у тебя текста налепились друг на друга потому что нет родительского Колумна.
-}
-
-@Composable
-fun Task2() {
-    // TODO: Сделай картинку, текст и кнопку которые располагаются посередине экрана
     Column {
-        Row {
-            Image(
-                painter = painterResource(id = R.drawable.gubkabob),
-                contentDescription = null, contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(23.dp))
-                    .width(100.dp)
-                    .height(100.dp)
 
 
-            )
-            Row {
-                Text(text = "Это текст?")
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Нет это патрик")
-                }
-            }
-        }
+        Text(text = "Вверх")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Середина ")
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Низ")
+        //тебе надо проверять задания в превью внизу. в этом задании у тебя текста налепились друг на друга потому что нет родительского Колумна.
     }
 }
 
 
 @Composable
+fun Task2() {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.gubkabob),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(RoundedCornerShape(23.dp))
+                .width(50.dp)
+                .height(50.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Это текст?")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Нет это патрик")
+            }
+        }
+    }
+}
+
+@Composable
 fun Task3() {
     // TODO: Сделай Колумн, в котором еще один колумн который 80х80, красного цвета и находится справа посередине экрана
     Column(Modifier.fillMaxSize()) {
+
         Column(
-            Modifier
+            modifier = Modifier
                 .width(80.dp)
                 .height(80.dp)
-                .background(Color.Red),
+                .background(Color.Red)
+                .align(Alignment.End),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.End
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
         }
@@ -116,14 +128,16 @@ fun Task4() {
 fun Task5() {
     // TODO: Выровняй 2 кнопки внутри Column по центру экрана.
     Column(
-        Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(onClick = { /*TODO*/ }) {
+            Text(text = "Кнопка 1")
         }
         Spacer(modifier = Modifier.height(10.dp))
         Button(onClick = { /*TODO*/ }) {
-
+            Text(text = "Кнопка 2")
         }
     }
 }
@@ -132,31 +146,43 @@ fun Task5() {
 fun Task6() {
     // TODO: Сделай Колумн который занимает весь экран, у него отступы со всех сторон 16.дп и он оранжевый
     // TODO: а внутри него 3 колумна белых, занимающих всё доступное пространство одинаковые по размеру и у них тоже пэддинг 16.дп
-    Column {
-        Modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-            .background(Color(0xFF800080))
-        Column {
-            Modifier
+            .background(Color(0xFFFFA500))
+    ) {
+
+        Column(
+            modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
                 .padding(16.dp)
                 .background(Color.White)
+        ) {
+
         }
-        Column {
-            Modifier
+
+
+        Column(
+            modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
                 .padding(16.dp)
                 .background(Color.White)
+        ) {
+
         }
-        Column {
-            Modifier
+
+
+        Column(
+            modifier = Modifier
                 .weight(1f)
                 .fillMaxWidth()
                 .padding(16.dp)
                 .background(Color.White)
+        ) {
+
         }
     }
 }
@@ -164,16 +190,19 @@ fun Task6() {
 @Composable
 fun Task7() {
     // TODO: Сделай родительский Колумн на весь экран. а в нем колумн 50х50 дп. Добавь дочернему Column рамку толщиной 2dp черного цвета.
-    Column {
-        Modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
             .border(2.dp, Color.Black)
+    ) {
         Column(
             modifier = Modifier
-                .size(50.dp)
                 .border(2.dp, Color.Black)
+                .size(50.dp)
                 .background(Color.White)
-        ) {}
+        ) {
+
+        }
     }
 }
 
@@ -182,15 +211,18 @@ fun Task8() {
     // TODO: Сделай родительский Column. В нем 3 Column, занимающие одинаковое пространство, с отступами 16.dp и разными цветами фона.
     // У каждого Column должно быть скругление углов. Внутри каждого Column размести 3 текста, что нибудь про любимую еду.
     // В первом Column используй Arrangement.Top, во втором - Arrangement.Center, в третьем - Arrangement.SpaceBetween.
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(
-                    shape = RoundedCornerShape(15.dp)
-                )
-                .background(Color.Cyan), verticalArrangement = Arrangement.Top
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.Cyan),
+            verticalArrangement = Arrangement.Top
         ) {
             Text(text = "Чай оооочень вкусный")
             Spacer(modifier = Modifier.height(20.dp))
@@ -198,12 +230,16 @@ fun Task8() {
             Spacer(modifier = Modifier.height(20.dp))
             Text(text = "Обожаю чай")
         }
+
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
-                .background(Color.Magenta), verticalArrangement = Arrangement.Center
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.Magenta),
+            verticalArrangement = Arrangement.Center
         ) {
             Text(text = "А какие я сегодня синабоны ел мммммм")
             Spacer(modifier = Modifier.height(20.dp))
@@ -211,13 +247,15 @@ fun Task8() {
             Spacer(modifier = Modifier.height(20.dp))
             Text(text = "Такой мягкий вкусный прям таял во рту")
         }
+
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
+                .clip(RoundedCornerShape(15.dp))
                 .background(Color.Unspecified),
-
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(text = "Мины в лаваше")
@@ -226,7 +264,6 @@ fun Task8() {
             Spacer(modifier = Modifier.height(20.dp))
             Text(text = "(я такое не люблю)")
         }
-
     }
 }
 
@@ -235,122 +272,142 @@ fun Task9() {
     // TODO: Сделай родительский Column. В нем 3 Column, занимающие одинаковое пространство, с отступами 16.dp и разными цветами фона.
     // У каждого Column должно быть скругление углов. Внутри каждого Column размести 3 текста, что нибудь про музыку.
     // В первом Column используй Alignment.Start, во втором - Alignment.CenterHorizontally, в третьем - Alignment.End.
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
-                .background(Color.Unspecified),
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.Magenta),
             horizontalAlignment = Alignment.Start
         ) {
             Text(text = "Хочу закончить одну песню")
             Spacer(modifier = Modifier.height(20.dp))
-            Text(text = "Ну по идеии она уже закончена ")
+            Text(text = "Ну по идеии она уже закончена")
             Spacer(modifier = Modifier.height(20.dp))
             Text(text = "Осталось сыграть")
-
-
         }
+
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
-                .background(Color.Unspecified),
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.DarkGray),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "Хочу закончить свою новую мелодию по быстрей")
             Spacer(modifier = Modifier.height(20.dp))
-            Text(text = "Довести ее до ума ")
+            Text(text = "Довести ее до ума")
             Spacer(modifier = Modifier.height(20.dp))
             Text(text = "И придумать слова")
         }
+
+
         Column(
-            Modifier
+            modifier = Modifier
+                .width(1000.dp)
                 .weight(1f)
                 .padding(16.dp)
-                .clip(shape = RoundedCornerShape(15.dp))
-                .background(Color.Unspecified),
+                .clip(RoundedCornerShape(15.dp))
+                .background(Color.Cyan),
             horizontalAlignment = Alignment.End
         ) {
             Text(text = "Стрiли Летять")
-            Text(text = " навстречу вечной тьме")
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(text = "Навстречу вечной тьме")
+            Spacer(modifier = Modifier.height(20.dp))
             Text(text = "В день, когда придёт домой")
-
         }
-
     }
+
 }
 
 
 @Composable
 fun Task10() {
-    // TODO: Сделай родительский Column. В нем 3 Column, занимающие одинаковое пространство, с отступами 16.dp и разными цветами фона.
-    // У каждого Column должно быть скругление углов. Внутри каждого Column размести 3 текста, что нибудь про науки)).
-    // В первом Column используй Arrangement.Top и Alignment.Start, во втором - Arrangement.Center и Alignment.CenterHorizontally, в третьем - Arrangement.SpaceBetween и Alignment.End.
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
         Column(
-            Modifier
-                .fillMaxSize()
+            modifier = Modifier
+                .width(1000.dp)
+                .weight(1f)
                 .padding(16.dp)
                 .clip(RoundedCornerShape(12.dp))
-                .background(Color.Unspecified)
+                .background(Color.LightGray),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
         ) {
             Text(text = "Пу пу")
+            Spacer(modifier = Modifier.height(10.dp))
             Text(text = "Кря кря")
+            Spacer(modifier = Modifier.height(10.dp))
             Text(text = "Это наука")
         }
+
+
         Column(
-            Modifier
-                .fillMaxSize()
+            modifier = Modifier
+                .width(1000.dp)
+                .weight(1f)
                 .padding(16.dp)
                 .clip(RoundedCornerShape(12.dp))
-                .background(Color.Unspecified)
+                .background(Color.Yellow),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "Проверим тебя")
-            Text(text = "Уровень:Изи")
-            Text(
-                text = "2+2*2"
-            )
-        }
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(16.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(Color.Unspecified)
-        ) {
-            Text(text = "Ооооо не смог то решить ну на по легче ")
-            Text(text = "Теперь Уровень:Решит даже моя Бабушка")
-            Text(text = "Шли два друго один в Киев другой на запад сколько будет стоит 1кг помидор если дерево рыбный суп ростом 3 метра ")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Уровень: Изи")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "2+2*2")
         }
 
+
+        Column(
+            modifier = Modifier
+                .width(1000.dp)
+                .weight(1f)
+                .padding(16.dp)
+                .clip(RoundedCornerShape(12.dp))
+                .background(Color.Cyan),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(text = "Ооооо не смог то решить, ну на попроще")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Теперь Уровень: Решит даже моя бабушка")
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = "Шли два друга, один в Киев, другой на запад. Сколько будет стоить 1 кг помидор, если дерево, рыбный суп и ростом 3 метра?")
+        }
     }
 }
+
 
 @Composable
 fun Task11() {
     // TODO: Создай Column, в котором будет много элементов, чтобы была вертикальная прокрутка.
     // TODO: Используй Modifier.verticalScroll() для прокрутки.
-    Column(
+
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    )
-    {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Black)
-                .padding(16.dp),
+            .background(Color.Black)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(30) { index ->
 
-            ) {
-            items(30) {
-                Skover()
-            }
+            Skover()
         }
     }
 }
@@ -436,7 +493,7 @@ fun Task13() {
 @Composable
 fun PreviewColumnTasks() {
     //сюда пишете задание которое сейчас делаете, чтоб смотреть что получилось
-    Task3()
+    Task13()
 }
 
 @Preview
